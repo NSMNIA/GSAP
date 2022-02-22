dbf99d174f9ed063a70a35c081133e9d263ac688
Kevin Stoop <433243@student.fontys.nl>
2022-02-22 17:25:00 +0100

    style updated
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- assets/js/index.js
+++ assets/js/index.js
@@ -88,7 +88,7 @@ if(document.querySelector('#scroll')){
 
      ScrollTrigger.scrollerProxy("main", {
       	scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : 		locoScroll.scroll.instance.scroll.y;
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
         },
         getBoundingClientRect() {
             return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
