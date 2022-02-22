d2d894b0081b1b4531c2995275511a51d0ff32b0
Kevin Stoop <433243@student.fontys.nl>
2022-02-22 13:35:35 +0100

    page animation added
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,31 @@
-document.body.addEventListener('mousemove', e => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+if(document.querySelector('#home')){
+    document.body.addEventListener('mousemove', e => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
 
-    gsap.set(".cursor", {
-        x: mouseX,
-        y: mouseY
-    });
+        gsap.set(".cursor", {
+            x: mouseX,
+            y: mouseY
+        });
 
-    gsap.to(".shape", {
-        x: mouseX,
-        y: mouseY,
-        stagger: -.1
+        gsap.to(".shape", {
+            x: mouseX,
+            y: mouseY,
+            stagger: -.1
+        });
     });
-});
+}
+
+if(document.querySelector('#index2')){
+    let rule = CSSRulePlugin.getRule(".anim1 span:after");
+    let tl = gsap.timeline({defaults: {duration: 1}});
+    tl.from('.anim1', {y: -50, stagger: .3, opacity: 0, clearProps: "all"})
+        .to(rule, {duration: 1.8, cssRule: {scaleY: 0}}, "-=2.2");
+    document.querySelector("#cta").addEventListener('click', e =>{
+        e.preventDefault();
+        tl.reversed() ? tl.play() : tl.reverse();
+        setTimeout(() => {
+            window.location.href = e.target.href;
+        }, 2200);
+    })
+}
--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@
 A fun text animation with GSAP and css mix-blend-mode :)
 
 
-Example: [https://nsmnia.github.io/GSAP/](https://nsmnia.github.io/GSAP/)
-
+Example 1: [https://nsmnia.github.io/GSAP/](https://nsmnia.github.io/GSAP/)
 
 ![End result](/assets/images/example.png)
+
+Example 2: [https://nsmnia.github.io/GSAP/index2.html](https://nsmnia.github.io/GSAP/index2.html)
